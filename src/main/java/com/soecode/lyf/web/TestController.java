aa10c184f4eb8b5060e19ed312dc8befc38df859
HbeH <467053290@qq.com>
2018-08-16 14:38:41 +0800

    init

---
+++ src/main/java/com/soecode/lyf/web/TestController.java
@@ -0,0 +1,22 @@
+package com.soecode.lyf.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.soecode.lyf.dto.BackResult;
+
+@Controller
+@ResponseBody
+public class TestController {
+
+	@RequestMapping("/test")
+	public BackResult test(){
+		System.out.println(22);
+		BackResult backResult = new BackResult();
+		backResult.setData("1");
+		backResult.setMsg("test");
+		backResult.setStatus(0);
+		return backResult;
+	}
+}
